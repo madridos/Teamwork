06380aaf82c1dac0d2bf5fb4dcba5e14473dad61
madridos <aghilesboutaleb7@gmail.com>
2020-05-12 05:27:54 -0400

    small change

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,11 @@
+import soccer.SoccerInfo;
+
+
 public class Test {
+    public static void main(String []args){
+
+        SoccerInfo.getCountry();
+        SoccerInfo si = new SoccerInfo("real madrid");
+
+    }
 }
